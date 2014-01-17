d02cd26efde306d8ef4aaf25e32d65f18b921498
Anders Ramsay <andersr@gmail.com>
2014-01-17 16:14:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mcards::Application.config.secret_key_base = '602383befd2bec5db500d137b1d4e0a3de51d22187294eabcc0e0f1784c8be4679ad756737a931cee185c08402fe481770d4b1ea76a2ccc2b158ab6839cf61ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mcards::Application.config.session_store :cookie_store, key: '_mcards_session'
