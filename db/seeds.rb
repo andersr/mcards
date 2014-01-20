f014e8ef7d419618649ef9f2e71fd782a153ea36
Anders Ramsay <andersr@gmail.com>
2014-01-20 14:11:09 -0500

    heroku deploy prep

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cards = Card.create([
+  {phrase:"Smile for no reason :-)"},
+  {phrase:"Breeeeathe..."},
+  {phrase:'Is there someone you are holding a grudge against? Take a minute and visualize this person in your mind, letting go of negative emotions.'},
+  {phrase:"Remember, joy comes from within. No one else can make you happy."},
+  {phrase:"Give yourself credit for a recent achievement. You are doing the best you can, every day."},
+  {phrase:"What is something you are dreaming of achieving?  Take a minute and visualize yourself having already achieved it."},
+  {phrase:"Take joy in the success of others."},
+  {phrase:"Today, decide to help someone without expecting anything in return"},
+  {phrase:"Decide today that you're going to react with kindness to people who annoy you."},
+  {phrase:"Is there someone who recently upset you in some way?  Take a minute and visualize them in your mind, letting go of negative emotions."},
+  {phrase:"Today, find one or more physical items in your home that you can give away to a charity."},
+  {phrase:"What is something you are dreaming of achieving? Think of one small step you can take today to bring you closer toward your goal."}
+  ])
