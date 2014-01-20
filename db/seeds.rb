ae03d127159ba4a9dd52e9830b6676c167d586ad
Anders Ramsay <andersr@gmail.com>
2014-01-20 14:28:26 -0500

    seed cards updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 cards = Card.create([
   {phrase:"Smile for no reason :-)"},
   {phrase:"Breeeeathe..."},
@@ -15,8 +7,9 @@ cards = Card.create([
   {phrase:"What is something you are dreaming of achieving?  Take a minute and visualize yourself having already achieved it."},
   {phrase:"Take joy in the success of others."},
   {phrase:"Today, decide to help someone without expecting anything in return"},
-  {phrase:"Decide today that you're going to react with kindness to people who annoy you."},
+  {phrase:"Today, try reacting with kindness to people who annoy you."},
   {phrase:"Is there someone who recently upset you in some way?  Take a minute and visualize them in your mind, letting go of negative emotions."},
   {phrase:"Today, find one or more physical items in your home that you can give away to a charity."},
-  {phrase:"What is something you are dreaming of achieving? Think of one small step you can take today to bring you closer toward your goal."}
+  {phrase:"What is something you are dreaming of achieving? Think of one small step you can take today to bring you closer toward your goal."},
+  {phrase:"Are you feeling anxious? Close your eyes and visualize your anxieties drifting away from you."}
   ])
